82f77f43cf16fd3bd6e48550214da91282f03118
Tem Taepaisitphongse <temt4510@gmail.com>
2025-04-15 23:43:32 -0400

    david's ver working, only player detection

---
+++ annotators/annotate.py
@@ -0,0 +1,128 @@
+from typing import Optional, List
+
+import cv2
+import supervision as sv
+import numpy as np
+
+from configs.soccer import BasketballCourtConfiguration
+
+def draw_basketball_court(
+    config: BasketballCourtConfiguration,
+    background_color: sv.Color = sv.Color(34, 139, 34),
+    line_color: sv.Color = sv.Color.WHITE,
+    padding: int = 50,
+    line_thickness: int = 4,
+    point_radius: int = 8,
+    scale: float = 0.1
+) -> np.ndarray:
+    """
+    Draws a basketball Court with specified dimensions, colors, and scale.
+
+    Args:
+        config (SoccerPitchConfiguration): Configuration object containing the
+            dimensions and layout of the pitch.
+        background_color (sv.Color, optional): Color of the pitch background.
+            Defaults to sv.Color(34, 139, 34).
+        line_color (sv.Color, optional): Color of the pitch lines.
+            Defaults to sv.Color.WHITE.
+        padding (int, optional): Padding around the pitch in pixels.
+            Defaults to 50.
+        line_thickness (int, optional): Thickness of the pitch lines in pixels.
+            Defaults to 4.
+        point_radius (int, optional): Radius of the penalty spot points in pixels.
+            Defaults to 8.
+        scale (float, optional): Scaling factor for the pitch dimensions.
+            Defaults to 0.1.
+
+    Returns:
+        np.ndarray: Image of the soccer pitch.
+    """
+    scaled_width = int(config.width * scale)
+    scaled_length = int(config.length * scale)
+
+    pitch_image = np.ones(
+        (scaled_width + 2 * padding,
+         scaled_length + 2 * padding, 3),
+        dtype=np.uint8
+    ) * np.array(background_color.as_bgr(), dtype=np.uint8)
+
+    for start, end in config.edges:
+        point1 = (int(config.vertices[start - 1][0] * scale) + padding,
+                  int(config.vertices[start - 1][1] * scale) + padding)
+        point2 = (int(config.vertices[end - 1][0] * scale) + padding,
+                  int(config.vertices[end - 1][1] * scale) + padding)
+        cv2.line(
+            img=pitch_image,
+            pt1=point1,
+            pt2=point2,
+            color=line_color.as_bgr(),
+            thickness=line_thickness
+        )
+
+    return pitch_image
+
+def draw_points_on_court(
+    config: BasketballCourtConfiguration,
+    xy: np.ndarray,
+    face_color: sv.Color = sv.Color.RED,
+    edge_color: sv.Color = sv.Color.BLACK,
+    radius: int = 10,
+    thickness: int = 2,
+    padding: int = 50,
+    scale: float = 0.1,
+    pitch: Optional[np.ndarray] = None
+) -> np.ndarray:
+    """
+    Draws points on a soccer pitch.
+
+    Args:
+        config (SoccerPitchConfiguration): Configuration object containing the
+            dimensions and layout of the pitch.
+        xy (np.ndarray): Array of points to be drawn, with each point represented by
+            its (x, y) coordinates.
+        face_color (sv.Color, optional): Color of the point faces.
+            Defaults to sv.Color.RED.
+        edge_color (sv.Color, optional): Color of the point edges.
+            Defaults to sv.Color.BLACK.
+        radius (int, optional): Radius of the points in pixels.
+            Defaults to 10.
+        thickness (int, optional): Thickness of the point edges in pixels.
+            Defaults to 2.
+        padding (int, optional): Padding around the pitch in pixels.
+            Defaults to 50.
+        scale (float, optional): Scaling factor for the pitch dimensions.
+            Defaults to 0.1.
+        pitch (Optional[np.ndarray], optional): Existing pitch image to draw points on.
+            If None, a new pitch will be created. Defaults to None.
+
+    Returns:
+        np.ndarray: Image of the soccer pitch with points drawn on it.
+    """
+    if pitch is None:
+        pitch = draw_basketball_court(
+            config=config,
+            padding=padding,
+            scale=scale
+        )
+
+    for point in xy:
+        scaled_point = (
+            int(point[0] * scale) + padding,
+            int(point[1] * scale) + padding
+        )
+        cv2.circle(
+            img=pitch,
+            center=scaled_point,
+            radius=radius,
+            color=face_color.as_bgr(),
+            thickness=-1
+        )
+        cv2.circle(
+            img=pitch,
+            center=scaled_point,
+            radius=radius,
+            color=edge_color.as_bgr(),
+            thickness=thickness
+        )
+
+    return pitch
---
+++ main.py
@@ -0,0 +1,355 @@
+import argparse
+from enum import Enum
+from typing import Iterator, List
+
+import os
+import cv2
+import numpy as np
+import supervision as sv
+from tqdm import tqdm
+from ultralytics import YOLO
+
+from common.team import TeamClassifier
+from common.ball import BallTracker, BallAnnotator
+from common.view import ViewTransformer
+from configs.soccer import BasketballCourtConfiguration
+from annotators.annotate import draw_basketball_court, draw_points_on_court
+
+import warnings
+warnings.filterwarnings("ignore", message="'force_all_finite' was renamed to 'ensure_all_finite'")
+
+PARENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+PLAYER_DETECTION_MODEL_PATH = os.path.join(PARENT_DIR, 'data/models/nba_detection.pt')
+KEYPOINT_DETECTION_MODEL_PATH = os.path.join(PARENT_DIR, 'data/models/keypoint2.pt')
+
+CONFIG = BasketballCourtConfiguration
+
+PLAYER_CLASS_ID = 1
+BALL_CLASS_ID = 0
+
+STRIDE = 60
+
+COLORS = ['#FF1493', '#00BFFF', '#FF6347', '#FFD700']
+
+BOX_ANNOTATOR = sv.BoxAnnotator(
+    color=sv.ColorPalette.from_hex(COLORS),
+    thickness=2
+)
+ELLIPSE_ANNOTATOR = sv.EllipseAnnotator(
+    color=sv.ColorPalette.from_hex(COLORS),
+    thickness=2
+)
+BOX_LABEL_ANNOTATOR = sv.LabelAnnotator(
+    color=sv.ColorPalette.from_hex(COLORS),
+    text_color=sv.Color.from_hex('#FFFFFF'),
+    text_padding=5,
+    text_thickness=1,
+)
+ELLIPSE_LABEL_ANNOTATOR = sv.LabelAnnotator(
+    color=sv.ColorPalette.from_hex(COLORS),
+    text_color=sv.Color.from_hex('#FFFFFF'),
+    text_padding=5,
+    text_thickness=1,
+    text_position=sv.Position.BOTTOM_CENTER,
+)
+
+class Mode(Enum):
+    """
+    Enum class representing different modes of operation for Soccer AI video analysis.
+    """
+    PITCH_DETECTION = 'PITCH_DETECTION'
+    PLAYER_DETECTION = 'PLAYER_DETECTION'
+    BALL_DETECTION = 'BALL_DETECTION'
+    PLAYER_TRACKING = 'PLAYER_TRACKING'
+    TEAM_CLASSIFICATION = 'TEAM_CLASSIFICATION'
+    RADAR = 'RADAR'
+
+def get_crops(frame: np.ndarray, detections: sv.Detections) -> List[np.ndarray]:
+    """
+    Extract crops from the frame based on detected bounding boxes.
+
+    Args:
+        frame (np.ndarray): The frame from which to extract crops.
+        detections (sv.Detections): Detected objects with bounding boxes.
+
+    Returns:
+        List[np.ndarray]: List of cropped images.
+    """
+    return [sv.crop_image(frame, xyxy) for xyxy in detections.xyxy]
+
+
+
+def run_player_detection(source_video_path: str, device: str) -> Iterator[np.ndarray]:
+    """
+    Run player detection on a video and yield annotated frames.
+
+    Args:
+        source_video_path (str): Path to the source video.
+        device (str): Device to run the model on (e.g., 'cpu', 'cuda').
+
+    Yields:
+        Iterator[np.ndarray]: Iterator over annotated frames.
+    """
+    player_detection_model = YOLO(PLAYER_DETECTION_MODEL_PATH).to(device=device)
+    frame_generator = sv.get_video_frames_generator(source_path=source_video_path)
+    for frame in frame_generator:
+        result = player_detection_model(frame, imgsz=1280, verbose=False)[0]
+        detections = sv.Detections.from_ultralytics(result)
+
+        annotated_frame = frame.copy()
+        annotated_frame = BOX_ANNOTATOR.annotate(annotated_frame, detections[detections.class_id == PLAYER_CLASS_ID])
+        annotated_frame = BOX_LABEL_ANNOTATOR.annotate(annotated_frame, detections[detections.class_id == PLAYER_CLASS_ID])
+        yield annotated_frame
+
+def run_player_tracking(source_video_path: str, device: str) -> Iterator[np.ndarray]:
+    """
+    Run player tracking on a video and yield annotated frames with tracked players.
+
+    Args:
+        source_video_path (str): Path to the source video.
+        device (str): Device to run the model on (e.g., 'cpu', 'cuda').
+
+    Yields:
+        Iterator[np.ndarray]: Iterator over annotated frames.
+    """
+    player_detection_model = YOLO(PLAYER_DETECTION_MODEL_PATH).to(device=device)
+    frame_generator = sv.get_video_frames_generator(source_path=source_video_path)
+    tracker = sv.ByteTrack(minimum_consecutive_frames=3)
+    for frame in frame_generator:
+        result = player_detection_model(frame, imgsz=1280, verbose=False)[0]
+        detections = sv.Detections.from_ultralytics(result)
+        detections = tracker.update_with_detections(detections[detections.class_id == PLAYER_CLASS_ID])
+
+        labels = [str(tracker_id) for tracker_id in detections.tracker_id]
+
+        annotated_frame = frame.copy()
+        annotated_frame = ELLIPSE_ANNOTATOR.annotate(annotated_frame, detections)
+        annotated_frame = ELLIPSE_LABEL_ANNOTATOR.annotate(
+            annotated_frame, detections, labels=labels)
+        yield annotated_frame
+
+def filter_refs(detections: sv.Detections, team_classifier: TeamClassifier, crops: List[np.ndarray]) -> sv.Detections:
+    team_classifier.predict(crops)
+
+
+def run_ball_detection(source_video_path: str, device: str) -> Iterator[np.ndarray]:
+    """
+    Run ball detection on a video and yield annotated frames.
+
+    Args:
+        source_video_path (str): Path to the source video.
+        device (str): Device to run the model on (e.g., 'cpu', 'cuda').
+
+    Yields:
+        Iterator[np.ndarray]: Iterator over annotated frames.
+    """
+    ball_detection_model = YOLO(PLAYER_DETECTION_MODEL_PATH).to(device=device)
+    frame_generator = sv.get_video_frames_generator(source_path=source_video_path)
+    
+    ball_tracker = BallTracker(buffer_size=20)
+    ball_annotator = BallAnnotator(radius=6, buffer_size=10)
+
+    def callback(image_slice: np.ndarray) -> sv.Detections:
+        result = ball_detection_model(image_slice, imgsz=640, verbose=False)[0]
+        detections = sv.Detections.from_ultralytics(result)
+        detections =  detections[detections.class_id == BALL_CLASS_ID]
+        return detections[detections.confidence > 0.5]
+
+    slicer = sv.InferenceSlicer(
+        callback=callback,
+        slice_wh=(640, 640),
+        overlap_wh=[0.3,0.3],
+        overlap_ratio_wh=None,
+    )
+
+    for frame in frame_generator:
+        detections = slicer(frame).with_nms(threshold=0.25)
+        detections = ball_tracker.update(detections)
+        annotated_frame = frame.copy()
+        annotated_frame = ball_annotator.annotate(annotated_frame, detections)
+        yield annotated_frame
+
+def run_team_classification(source_video_path: str, device: str) -> Iterator[np.ndarray]:
+    """
+    Run team classification on a video and yield annotated frames with team colors.
+
+    Args:
+        source_video_path (str): Path to the source video.
+        device (str): Device to run the model on (e.g., 'cpu', 'cuda').
+
+    Yields:
+        Iterator[np.ndarray]: Iterator over annotated frames.
+    """
+    player_detection_model = YOLO(PLAYER_DETECTION_MODEL_PATH).to(device=device)
+    frame_generator = sv.get_video_frames_generator(
+        source_path=source_video_path, stride=STRIDE)
+
+    crops = []
+    for frame in tqdm(frame_generator, desc='collecting crops'):
+        result = player_detection_model(frame, imgsz=1280, verbose=False)[0]
+        detections = sv.Detections.from_ultralytics(result)
+        crops += get_crops(frame, detections[detections.class_id == PLAYER_CLASS_ID])
+
+    team_classifier = TeamClassifier(device=device)
+    team_classifier.fit(crops)
+
+    frame_generator = sv.get_video_frames_generator(source_path=source_video_path)
+    tracker = sv.ByteTrack(minimum_consecutive_frames=3)
+    for frame in frame_generator:
+        result = player_detection_model(frame, imgsz=1280, verbose=False)[0]
+        detections = sv.Detections.from_ultralytics(result)
+        detections = tracker.update_with_detections(detections)
+
+        players = detections[detections.class_id == PLAYER_CLASS_ID]
+        crops = get_crops(frame, players)
+
+
+        players_team_id = team_classifier.predict(crops)
+
+        color_lookup = np.array(
+                players_team_id.tolist()
+        )
+        labels = [str(tracker_id) for tracker_id in players.tracker_id]
+
+        annotated_frame = frame.copy()
+        annotated_frame = ELLIPSE_ANNOTATOR.annotate(
+            annotated_frame, players, custom_color_lookup=color_lookup)
+        annotated_frame = ELLIPSE_LABEL_ANNOTATOR.annotate(
+            annotated_frame, players, labels, custom_color_lookup=color_lookup)
+        yield annotated_frame
+
+def render_radar(
+    detections: sv.Detections,
+    keypoints: sv.KeyPoints,
+    color_lookup: np.ndarray
+) -> np.ndarray:
+    mas = (keypoints.xy[0][:, 0] > 1) & (keypoints.xy[0][:, 1] > 1)
+    transformer = ViewTransformer(
+        source=keypoints.xy[0][mask].astype(np.float32),
+        target=np.array(CONFIG.vertices)[mask].astype(np.float32)
+    )
+    xy = detections.get_anchors_coordinates(anchor=sv.Position.BOTTOM_CENTER)
+    transformed_xy = transformer.transform_points(points=xy)
+
+    radar = draw_basketball_court(config=CONFIG)
+    radar = draw_points_on_court(
+        config=CONFIG, xy=transformed_xy[color_lookup == 0],
+        face_color=sv.Color.from_hex(COLORS[0]), radius=20, pitch=radar)
+    radar = draw_points_on_court(
+        config=CONFIG, xy=transformed_xy[color_lookup == 1],
+        face_color=sv.Color.from_hex(COLORS[1]), radius=20, pitch=radar)
+    radar = draw_points_on_court(
+        config=CONFIG, xy=transformed_xy[color_lookup == 2],
+        face_color=sv.Color.from_hex(COLORS[2]), radius=20, pitch=radar)
+    # radar = draw_points_on_pitch(
+    #     config=CONFIG, xy=transformed_xy[color_lookup == 3],
+    #     face_color=sv.Color.from_hex(COLORS[3]), radius=20, pitch=radar)
+    return radar
+
+def run_radar(source_video_path: str, device: str) -> Iterator[np.ndarray]:
+    player_detection_model = YOLO(PLAYER_DETECTION_MODEL_PATH).to(device=device)
+    court_detection_model = YOLO(KEYPOINT_DETECTION_MODEL_PATH).to(device=device)
+    frame_generator = sv.get_video_frames_generator(
+        source_path=source_video_path, stride=STRIDE)
+
+    crops = []
+    for frame in tqdm(frame_generator, desc='collecting crops'):
+        result = player_detection_model(frame, imgsz=1280, verbose=False)[0]
+        detections = sv.Detections.from_ultralytics(result)
+        crops += get_crops(frame, detections[detections.class_id == PLAYER_CLASS_ID])
+
+    team_classifier = TeamClassifier(device=device)
+    team_classifier.fit(crops)
+
+    frame_generator = sv.get_video_frames_generator(source_path=source_video_path)
+    tracker = sv.ByteTrack(minimum_consecutive_frames=3)
+    for frame in frame_generator:
+        result = court_detection_model(frame, verbose=False)[0]
+        keypoints = sv.Detections.from_ultralytics(result)
+        
+        
+
+        result = player_detection_model(frame, imgsz=1280, verbose=False)[0]
+        detections = sv.Detections.from_ultralytics(result)
+        detections = tracker.update_with_detections(detections)
+
+        players = detections[detections.class_id == PLAYER_CLASS_ID]
+        crops = get_crops(frame, players)
+        players_team_id = team_classifier.predict(crops)
+
+
+        detections = sv.Detections.merge([players])
+        color_lookup = np.array(
+            players_team_id.tolist()
+        )
+        labels = [str(tracker_id) for tracker_id in detections.tracker_id]
+
+        annotated_frame = frame.copy()
+        annotated_frame = ELLIPSE_ANNOTATOR.annotate(
+            annotated_frame, detections, custom_color_lookup=color_lookup)
+        annotated_frame = ELLIPSE_LABEL_ANNOTATOR.annotate(
+            annotated_frame, detections, labels,
+            custom_color_lookup=color_lookup)
+
+        h, w, _ = frame.shape
+        radar = render_radar(detections, keypoints, color_lookup)
+        radar = sv.resize_image(radar, (w // 2, h // 2))
+        radar_h, radar_w, _ = radar.shape
+        rect = sv.Rect(
+            x=w // 2 - radar_w // 2,
+            y=h - radar_h,
+            width=radar_w,
+            height=radar_h
+        )
+        annotated_frame = sv.draw_image(annotated_frame, radar, opacity=0.5, rect=rect)
+        yield annotated_frame
+
+
+def main(source_video_path: str, target_video_path: str, device: str, mode: Mode) -> None:
+
+    # if mode == Mode.PITCH_DETECTION:
+    #     frame_generator = run_pitch_detection(
+    #         source_video_path=source_video_path, device=device)
+    if mode == Mode.PLAYER_DETECTION:
+        frame_generator = run_player_detection(
+            source_video_path=source_video_path, device=device)
+    elif mode == Mode.BALL_DETECTION:
+        frame_generator = run_ball_detection(
+            source_video_path=source_video_path, device=device)
+    elif mode == Mode.PLAYER_TRACKING:
+        frame_generator = run_player_tracking(
+            source_video_path=source_video_path, device=device)
+    elif mode == Mode.TEAM_CLASSIFICATION:
+        frame_generator = run_team_classification(
+            source_video_path=source_video_path, device=device)
+    elif mode == Mode.RADAR:
+        frame_generator = run_radar(
+            source_video_path=source_video_path, device=device)
+    else:
+        raise NotImplementedError(f"Mode {mode} is not implemented.")
+
+    video_info = sv.VideoInfo.from_video_path(source_video_path)
+    with sv.VideoSink(target_video_path, video_info) as sink:
+        for frame in frame_generator:
+            sink.write_frame(frame)
+
+            cv2.imshow("frame", frame)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--source_video_path', type=str, required=True)
+    parser.add_argument('--target_video_path', type=str, default ='output.mp4')
+    parser.add_argument('--device', type=str, default='cpu')
+    parser.add_argument('--mode', type=Mode, default=Mode.RADAR)
+    args = parser.parse_args()
+    main(
+        source_video_path=args.source_video_path,
+        target_video_path=args.target_video_path,
+        device=args.device,
+        mode=args.mode
+    )
